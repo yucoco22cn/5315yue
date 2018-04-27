1ea236d7a1318fb5d5e4ff3011de74a6891b3371
yue <yucoco22cn@gmail.com>
2018-04-27 11:14:48 -0700

    h

--- HW2data.js
+++ HW2data.js
@@ -4,77 +4,77 @@
       "author": "Gambardella, Matthew",
       "book": "XML Developer's Guide",
       "genre": "Computer",
-      "price": "44.95",
+      "price": "$44.95",
       "publish_date": "2000-10-01"
     },
     { 
       "author": "Ralls, Kim",
       "book": "Midnight Rain",
       "genre": "Fantasy",
-      "price": "5.95",
+      "price": "$5.95",
       "publish_date": "2000-12-16"
     },
     {
       "author": "Corets, Eva",
       "book": "Oberon's Legacy",
       "genre": "Fantasy",
-      "price": "5.95",
+      "price": "$5.95",
       "publish_date": "2001-03-10"
     },
     {
       "author": "Corets, Eva",
       "book": "The Sundered Grail",
       "genre": "Fantasy",
-      "price": "5.95",
+      "price": "$5.95",
       "publish_date": "2001-09-10"
     },
     {
       "author": "Randall, Cynthia",
       "book": "Lover Birds",
       "genre": "Romance",
-      "price": "4.95",
+      "price": "$4.95",
       "publish_date": "2000-09-02"
     },
     {
       "author": "Thurman, Paula",
       "book": "Splish Splash",
       "genre": "Romance",
-      "price": "4.95",
+      "price": "$4.95",
       "publish_date": "2000-11-02"
     },
     {
       "author": "Knorr, Stefan",
       "book": "Creepy Crawlies",
       "genre": "Horror",
-      "price": "4.95",
+      "price": "$4.95",
       "publish_date": "2000-12-06"
     },
     {
       "author": "Kress, Peter",
       "book": "Paradox Lost",
       "genre": "Science Fiction",
-      "price": "6.95",
+      "price": "$6.95",
       "publish_date": "2000-11-02"
     },
     {
       "author": "O'Brien, Tim",
       "book": "MSXML3: A Comprehensive Guide",
       "genre": "Computer",
-      "price": "36.95",
+      "price": "$36.95",
       "publish_date": "2000-12-01"
     },
     {
       "author": "O'Brien, Tim",
       "book": "Microsoft .NET: The Programming Bible",
       "genre": "Computer",
-      "price": "36.95",
+      "price": "$36.95",
       "publish_date": "2000-12-09"
     },
     {
       "author": "Galos, Mike",
       "book": "Visual Studio 7: A Comprehensive Guide",
       "genre": "Computer",
-      "price": "49.95",
+      "price": "$49.95",
       "publish_date": "2001-04-16"
     }
   ]
